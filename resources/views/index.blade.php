10cbb559545e0f5bf0494180c7fd9035d0189a48
andito19 <bintangandito@gmail.com>
2018-10-23 14:42:21 +0800

    Update APP Blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -95,7 +95,7 @@
       <!-- End Navbar -->
       <div class="content">
         <div class="container-fluid">
-          <!-- your content here -->
+          @yield('content')
         </div>
       </div>
     </div>
