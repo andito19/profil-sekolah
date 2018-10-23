d9cfd1f2e1cd93653158c7a2dee30f0ab35c5df6
ayuni putri <ayniptr@gmail.com>
2018-10-23 13:10:47 +0700

    nothing

---
+++ app/Http/Controllers/KelasController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KelasController extends Controller
+{
+    public function index()
+    {
+    	
+    }
+
+    public function create()
+    {
+    	# code...
+    }
+
+    public function save()
+    {
+    	# code...
+    }
+
+    public function edit()
+    {
+    	# code...
+    }
+
+    public function update()
+    {
+    	# code...
+    }
+
+    public function delete()
+    {
+    	# code...
+    }
+}
