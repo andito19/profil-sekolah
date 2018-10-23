65b3daaf2a2b81b718b897bc47a54955ec5167df
ayuni putri <ayniptr@gmail.com>
2018-10-23 13:14:17 +0700

    try create

--- app/Http/Controllers/KelasController.php
+++ app/Http/Controllers/KelasController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class KelasController extends Controller
 {
-    //
+    public function index()
+    {
+    	return view('Kelas.index');
+    }
+    public function create()
+    {
+    	return view('Kelas.create');
+    }
+    public function save()
+    {
+    	
+    }
+    public function edit()
+    {
+    	
+    }
+    public function update()
+    {
+    	
+    }
+    public function delete()
+    {
+    	
+    }
 }
