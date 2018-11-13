5ed2fae78feea8dd6c49d80ea21f4c1d655fdcad
mytung <mytung>
2018-11-13 13:34:40 +0700

    udah ye

---
+++ app/Http/Controllers/PrestasiController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Prestasi;
+
+class PrestasiController extends Controller
+{
+    public function index(){
+    	$data['prestasis'] = DB::table('prestasis')->orderBy('id', 'desc')->get();
+    	return view('prestasi.index',$data);
+    }
+    public function create(){
+    	return view('prestasi.create');
+    }
+    public function edit($Id){
+    	$data['prestasi'] = Prestasi::where('id', '=', $Id)->get();
+    	return view ('prestasi.edit',$data);
+    }
+    public function save(Request $r){
+    	$prestasi = new Prestasi;
+    	$prestasi->judul = $r->judulprestasi;
+    	$prestasi->descripsi = $r->deskripsiprestasi;
+    	$prestasi->save();
+
+    	return redirect()->route('prestasi.index');
+    }
+    public function deletePrestasi(Request $r){
+    	$delete = Prestasi::where("Id",$r->idprestasi)->delete();
+    	return redirect()->route('prestasi.index');
+    }
+    public function editSave(Request $r){
+    
+    	DB::table('prestasis')->where('id',$r->input('idprestasi'))->update(
+    		['judul' => $r->input('judulprestasi'),
+    		'descripsi' => $r->input('deskripsiprestasi')
+    	]
+    );
+        return redirect()->route('prestasi.index');
+    }
+    }
---
+++ routes/web.php
@@ -0,0 +1,102 @@
+w<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+
+Route::group(['prefix'=>'kelas','as'=>'kelas'],function(){
+	Route::get('/','KelasController@index')->name('index');
+	Route::get('/create','KelasController@create')->name('create');
+	Route::post('save','KelasController@save')->name('save');
+	Route::get('/edit/{id}','KelasController@edit')->name('edit');
+	Route::post('/update','KelasController@update')->name('update');
+	Route::get('/delete','KelasController@delete')->name('delete');	
+});
+
+
+Route::group(['prefix'=>'prestasi','as'=>'prestasi.'],function(){
+	Route::get('/','PrestasiController@index')->name('index');
+	Route::get('/create','PrestasiController@create')->name('create');
+	Route::post('save','PrestasiController@save')->name('save');
+	Route::get('/edit/{id}','PrestasiController@edit')->name('edit');
+	Route::post('/prestasi/edit/','PrestasiController@editSave')->name('editsave');
+	Route::post('/update','PrestasiController@update')->name('update');
+	Route::get('/delete','PrestasiController@delete')->name('delete');	
+	Route::post('/prestasi/delete','PrestasiController@deletePrestasi')->name('deleted');
+});
+
+
+Route::group(['prefix'=>'pengumuman','as'=>'pengumuman'],function(){
+	Route::get('/','PengumumanController@index')->name('index');
+	Route::get('/create','PengumumanController@create')->name('create');
+	Route::post('save','PengumumanController@save')->name('save');
+	Route::get('/edit/{id}','PengumumanController@edit')->name('edit');
+	Route::post('/update','PengumumanController@update')->name('update');
+	Route::get('/delete','PengumumanController@delete')->name('delete');	
+});
+
+
+
+Route::group(['prefix'=>'staff','as'=>'staff'],function(){
+	Route::get('/','StaffController@index')->name('index');
+	Route::get('/create','StaffController@create')->name('create');
+	Route::post('save','StaffController@save')->name('save');
+	Route::get('/edit/{id}','StaffController@edit')->name('edit');
+	Route::post('/update','StaffController@update')->name('update');
+	Route::get('/delete','StaffController@delete')->name('delete');	
+});
+
+
+
+Route::group(['prefix'=>'acara','as'=>'acara'],function(){
+	Route::get('/','AcaraController@index')->name('index');
+	Route::get('/create','AcaraController@create')->name('create');
+	Route::post('save','AcaraController@save')->name('save');
+	Route::get('/edit/{id}','AcaraController@edit')->name('edit');
+	Route::post('/update','AcaraController@update')->name('update');
+	Route::get('/delete','AcaraController@delete')->name('delete');	
+});
+
+
+
+
+Route::group(['prefix'=>'guru','as'=>'guru'],function(){
+	Route::get('/','GuruController@index')->name('index');
+	Route::get('/create','GuruController@create')->name('create');
+	Route::post('save','GuruController@save')->name('save');
+	Route::get('/edit/{id}','GuruController@edit')->name('edit');
+	Route::post('/update','GuruController@update')->name('update');
+	Route::get('/delete','GuruController@delete')->name('delete');	
+});
+
+
+Route::group(['prefix'=>'siswa','as'=>'siswa'],function(){
+	Route::get('/','SiswaController@index')->name('index');
+	Route::get('/create','SiswaController@create')->name('create');
+	Route::post('save','SiswaController@save')->name('save');
+	Route::get('/edit/{id}','SiswaKelasController@edit')->name('edit');
+	Route::post('/update','SiswaController@update')->name('update');
+	Route::get('/delete','SiswaController@delete')->name('delete');	
+});
+
+
+
+
+
+
